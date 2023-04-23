8c58d50ee2d8caf37735b5f60aa4a028371476a7
Keshanth Jude <keshanth64@gmail.com>
2023-04-23 12:55:41 +0100

    Moved python code to seperate file

--- data.py
+++ data.py
@@ -1,10 +1,22 @@
+from js import document, Element
 import requests
+import pyodide_http
+pyodide_http.patch_all()
 
-def get_public_repos(username):
+def get_public_repos():
+    username = document.getElementById("username").value
     url = f"https://api.github.com/users/{username}/repos"
     response = requests.get(url)
     if response.status_code == 200:
         repos = response.json()
-        return repos
+        repo_element = document.getElementById("manual-write")#
+
+        html = ""
+        for repo in repos:
+            repo_name = repo["name"]
+            repo_url = repo["html_url"]
+            html += f"""<a href="{repo_url}">{repo_name}</a><br>"""
+
+        repo_element.innerHTML = html
     else:
         return None
